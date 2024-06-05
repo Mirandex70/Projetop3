f61e1391a31611265386b4bcec5e681fd4745518
Mirandex70 <jtomasmiranda@gmail.com>
2024-06-05 22:36:35 +0100

    erros

--- backend/models/imagem.model.js
+++ backend/models/imagem.model.js
@@ -1,31 +1,32 @@
-const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config.js');
+const { DataTypes } = require('sequelize');
 
 const Imagem = sequelize.define('imagens', {
-  id_imagem: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  id_carro: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'carro',
-      key: 'id_carro',
+    id_imagem: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-  },
-  imagem_nome: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  imagem_url: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  timestamps: false,
-  tableName: 'imagens',
-});
-
-module.exports = Imagem;
+    id_carro: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'carro',
+        key: 'id_carro',
+      },
+    },
+    imagem_nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagem_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false,
+    tableName: 'imagens',
+  });
+  
+  module.exports = Imagem;
+  
--- backend/routes/imagens.route.js
+++ backend/routes/imagens.route.js
@@ -5,10 +5,20 @@ const imagensController = require('../controllers/imagens.controller.js');
 // Rota para criar uma nova imagem
 router.post('/', imagensController.create);
 
+// Rota para obter todas as imagens
+router.get('/', imagensController.findAll);
+
+// Rota para obter uma imagem pelo ID
+router.get('/:id_imagem', imagensController.findById);
+
+// Rota para atualizar uma imagem pelo ID
+router.put('/:id_imagem', imagensController.update);
+
 // Rota para obter todas as imagens relacionadas a um carro específico
-router.get('/:id_carro', imagensController.findAllByCar);
+router.get('/carro/:id_carro', imagensController.findAllByCar);
 
 // Rota para apagar uma imagem pelo ID
 router.delete('/:id_imagem', imagensController.delete);
+
 
 module.exports = router;
